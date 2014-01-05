e73231f5b56649b606ad0c3ce85b71dd03ad83b1
stefan <stefan_verhoeff@yahoo.com>
2014-01-05 16:56:57 +0100

    Got chat and socket.io

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ app.get("/", function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
 console.log("Listening on port " + port);
