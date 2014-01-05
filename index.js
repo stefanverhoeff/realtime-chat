1fe78489e2f1cb0373c9b4de1d54c3158ddb43bc
stefan <stefan_verhoeff@yahoo.com>
2014-01-05 16:32:30 +0100

    First hello page using Jade

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+console.log("Listening on port " + port);
