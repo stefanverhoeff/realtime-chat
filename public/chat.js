6fb42cded4e180c7891b75d34068a817d560fee4
stefan <stefan_verhoeff@yahoo.com>
2014-01-05 17:17:31 +0100

    Can set your name now

--- public/chat.js
+++ public/chat.js
@@ -3,16 +3,19 @@ window.onload = function() {
     var port = 3000;
     var messages = [];
     var socket = io.connect('http://localhost:' + port);
+    var name = document.getElementById("name");
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
 
+    var ls = localStorage;
+
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += '> ' + messages[i] + '<br />';
+                html += messages[i].person + '> ' + messages[i].message + '<br />';
             }
             content.innerHTML = html;
         } else {
@@ -20,14 +23,25 @@ window.onload = function() {
         }
     });
 
+
+    function sendChatMessage() {
+        var person = name.value;
+        var color = ls.color;
+        var text = field.value;
+
+        socket.emit('send', { person: person, color: color, message: text});
+
+        field.value = '';
+    }
+
+    sendButton.onclick = sendChatMessage;
+
     field.onkeydown = function (evt) {
         var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+
         if (keyCode == 13) {
             // For Enter.
-                var text = field.value;
-                socket.emit('send', { message: text });
-
-                field.value = '';
+            sendChatMessage();
         }
 
         if (keyCode == 27) {
@@ -36,7 +50,17 @@ window.onload = function() {
         }
     };
 
+    name.onchange = function () {
+        ls.name = this.value;
+    };
 
+    // Set chatter name
+    if (! ls.name) {
+        ls.name = 'Person ' + ('' + Math.random()) .substr(3, 3);
+    }
+
+    name.value = ls.name;
+
+    // Ready to chat
     field.focus();
-
 };
